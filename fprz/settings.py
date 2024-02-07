f3f142022122be90b335a5b0222e104915b44a7a
LaKosta1985 <xxxascent@rambler.ru>
2024-02-07 09:59:39 +0300

    полнофункционаьный текстовый поиск

--- fprz/settings.py
+++ fprz/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
     'main',
     "debug_toolbar",
     
@@ -79,8 +80,12 @@ WSGI_APPLICATION = 'fprz.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'fprz',
+        'USER':'admin',
+        'PASSWORD':'25111985admin',
+        'HOST': 'localhost',
+        "PORT":'5432',
     }
 }
 
--- fprz/utils.py
+++ fprz/utils.py
@@ -1,4 +1,19 @@
 from main.models import New
 from django.db.models import Q
+from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVector
 def q_search(query):
-    return New.objects.filter(title__icontains=query)
+
+    vector = SearchVector('title','content')
+    query = SearchQuery(query)
+    return New.objects.annotate(search=SearchRank(vector,query)).order_by("-rank")
+
+
+
+'''
+    keywords=[word for word in query.split() if len(word)>2]
+    q_objects=Q()
+    for token in keywords:
+        q_objects |=Q(title__icontains=token)
+        q_objects |=Q(content__icontains=token)
+    return New.objects.filter(q_objects)
+'''
